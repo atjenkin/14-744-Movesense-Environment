07da0ccf816be3a68e9ec50a51d20cbb74f0e7e1
atjenkin <atjenkin@andrew.cmu.edu>
2023-01-23 16:57:51 -0800

    removed device lib

--- movesense-device-lib/MovesenseCoreLib/include/ApplicationServices.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Suunto Oy 2016. All rights reserved.
-#pragma once
-
-//#include "ble/bleInterface.hpp"
-//#include "ble/comm/UartProtocol.hpp"
-
-#include "hwconfig.h"
-
-#include <whiteboard/LaunchableModule.h>
